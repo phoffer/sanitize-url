cd4b111797a6e071d02bfd5205ebb80fb3960c7d
Paul Hoffer <git@paulhoffer.dev>
2025-10-04 17:28:18 -0700

    lint

--- spec/char_codes_spec.rb
+++ spec/char_codes_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe 'Char codes' do
-  it 'counts a number as being in the range 48-57' do
+RSpec.describe "Char codes" do
+  it "counts a number as being in the range 48-57" do
     (0..9).each do |num|
       c = num.to_s
       code = c[0].is_a?(String) ? c.ord : c[0]
@@ -9,21 +9,21 @@ RSpec.describe 'Char codes' do
     end
   end
 
-  it 'counts an uppercase letter as being in the range 65-90' do
-    ('A'..'Z').each_with_index do |c, offset|
+  it "counts an uppercase letter as being in the range 65-90" do
+    ("A".."Z").each_with_index do |c, offset|
       code = c[0].is_a?(String) ? c.ord : c[0]
       expect(code).to eq(65 + offset)
     end
   end
 
-  it 'counts a lowercase letter as being in the range 97-122' do
-    ('a'..'z').each_with_index do |c, offset|
+  it "counts a lowercase letter as being in the range 97-122" do
+    ("a".."z").each_with_index do |c, offset|
       code = c[0].is_a?(String) ? c.ord : c[0]
       expect(code).to eq(97 + offset)
     end
   end
 
-  ['!', '*', "'", '(', ')', ';', ':', '@', '&', '=', '+', '$', ',', '/', '?', '%', '#', '[', ']', '-', '_', '.', '~'].each do |c|
+  ["!", "*", "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]", "-", "_", ".", "~"].each do |c|
     it "counts #{c} as included in VALID_OPAQUE_CHAR_CODES" do
       code = c[0].is_a?(String) ? c.ord : c[0]
       expect(SanitizeUrl::VALID_OPAQUE_CHAR_CODES).to include(code)
